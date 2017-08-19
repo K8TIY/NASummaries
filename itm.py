0b814e16c2f70b9cbd4da86ce74d0b4a412ec27b
K8TIY <moses@blugs.com>
2017-08-19 17:51:19 -0400

    Change from Symbola to Lucida Grande.

--- itm.py
+++ itm.py
@@ -29,7 +29,7 @@ def latexHeader(f):
 \newcommand{\scmono}[1]{{\fontspec{Source Code Pro}#1}}
 \newcommand{\cjk}[1]{{\fontspec[Scale=0.9]{Hiragino Mincho Pro}#1}}
 \newcommand{\asymbol}[1]{{\fontspec[Scale=0.9]{Apple Symbols}#1}}
-\newcommand{\symbola}[1]{{\fontspec[Scale=0.9]{Symbola}#1}}
+\newcommand{\lgrande}[1]{{\fontspec[Scale=0.9]{Lucida Grande}#1}}
 \addtolength{\oddsidemargin}{-.6in}
 \addtolength{\evensidemargin}{-.6in}
 \addtolength{\textwidth}{1.2in}
@@ -113,7 +113,8 @@ def latexEscape(s):
   s = re.sub(u'([\u0400-\u052F]+)', r'\\doulos{\1}', s)
   s = re.sub(u'([\u0370-\u03FF]+)', r'\\doulos{\1}', s)
   s = re.sub(u'([\u16A0-\u16FF]+)', r'\\asymbol{\1}', s)
-  s = re.sub(u'([\u2700-\u27BF]+)', r'\\symbola{\1}', s)
+  s = re.sub(u'([\u2700-\u27BF]+)', r'\\lgrande{\1}', s)
+  s = re.sub(u'([\u20A0-\u20CF]+)', r'\\lgrande{\1}', s)
   s = re.sub(r'__(.+?)__', r'\\cjk{\1}', s)
   s = re.sub(r'____', r'\\underline{\\hspace{2em}}', s)
   s = re.sub(r'\\&ast;', '*', s)
