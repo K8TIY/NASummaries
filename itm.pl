d547a186310f5cc6f4951511ebd63a28a71bc4fd
Moses Hall <moses@blugs.com>
2020-11-10 11:10:01 -0500

    Further debrittlization of show art img URL

--- itm.pl
+++ itm.pl
@@ -321,7 +321,7 @@ sub GetAlbumArt
                 ' Gecko/20100401 Firefox/3.6.3" --compressed '. $url;
       print BLUE "$cmd\n" if $opt_verbose;
       my $html = `$cmd`;
-      if ($html =~ m!(https?://.+?/enc/.+?-art-big.png)!)
+      if ($html =~ m!(https?://.+?/enc/.+?-art-big(-copy)?.png)!)
       {
         $arturl = $1;
       }
